a691e01ba26f2b262c267563ae7bccb80d1e55d5
sh4nnongoh <sh4nnongoh@gmail.com>
2023-04-09 20:22:35 -0400

    feat: update setup for latest nextjs support

--- setup.js
+++ setup.js
@@ -4,18 +4,16 @@ const { setJsonFileProps } = require("./lib/setJsonFileProps.js");
 const gitIgnore = fs.readFileSync(".gitignore", { encoding: "utf8" });
 const updatedGitIgnore = `${gitIgnore}\nbootstrap-next-typescript`;
 fs.writeFileSync(".gitignore", updatedGitIgnore);
+execSync("rm .eslintrc.json src/pages/_app.tsx src/pages/_document.tsx src/pages/index.tsx");
 execSync("mkdir -p src src/hooks src/contexts src/ui src/lib src/tests src/types");
-execSync("mv styles src/");
-execSync("rm -rf pages");
 execSync("cp -r ./bootstrap-next-typescript/setup/* .");
 execSync("cp -r ./bootstrap-next-typescript/setup/.vscode .");
 execSync("cp ./bootstrap-next-typescript/setup/.eslintignore .");
 execSync("cp ./bootstrap-next-typescript/setup/.eslintrc.json .");
+execSync("cp ./bootstrap-next-typescript/setup/.lintstagedrc .");
 execSync("yarn add lodash dotenv");
-execSync("yarn add -D tailwindcss postcss autoprefixer");
-execSync("yarn add -D install-peerdeps cross-env husky");
+execSync("yarn add -D install-peerdeps cross-env husky lint-staged");
 execSync("yarn install-peerdeps -D eslint-config-airbnb --yarn");
-execSync("yarn add -D eslint-config-next");
 execSync("yarn add -D jest jest-environment-jsdom ts-jest @types/jest @testing-library/react @testing-library/jest-dom @testing-library/user-event @types/lodash supertest @types/supertest");
 setJsonFileProps({
   filePath: "package.json",
@@ -33,12 +31,9 @@ setJsonFileProps({
 const app = fs.readFileSync("src/pages/_app.tsx", { encoding: "utf8" });
 const updatedApp = `/* eslint-disable react/jsx-props-no-spreading */\n${app}`;
 fs.writeFileSync("src/pages/_app.tsx", updatedApp);
-const globalCss = fs.readFileSync("src/styles/globals.css", { encoding: "utf8" });
-const updatedGlobalCss = `@tailwind base;\n@tailwind components;\n@tailwind utilities;\n\n${globalCss}`;
-fs.writeFileSync("src/styles/globals.css", updatedGlobalCss);
 execSync("yarn lint:fix");
 execSync("yarn husky install");
-execSync("yarn husky add .husky/pre-commit \"yarn lint && yarn test\"");
+execSync("yarn husky add .husky/pre-commit \"npx lint-staged && yarn test\"");
 execSync("yarn husky add .husky/pre-commit \"yarn update-version && git add package.json\"");
 execSync("git reset");
 execSync("git add .");
